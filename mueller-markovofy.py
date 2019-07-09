86f37a8b2305326d09bf2b054841e605b0b089b7
Cyrus Sethna <csethna@gmail.com>
2019-07-09 17:21:23 -0400

    pycon playing

--- mueller-markovofy.py
+++ mueller-markovofy.py
@@ -10,7 +10,7 @@ text_model = markovify.Text(text)
 # # Print five randomly-generated sentences
 # for i in range(5):
 #     print(text_model.make_sentence())
-
-# Print three randomly-generated sentences of no more than 140 characters
+#
+# # Print three randomly-generated sentences of no more than 140 characters
 for i in range(15):
     print(text_model.make_short_sentence(240))
